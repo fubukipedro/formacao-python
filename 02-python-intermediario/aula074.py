b6a6b6a9b3f18f92a4838a69c7e7a6c94c170167
Pedro Henrique <pedrohfubuki@gmail.com>
2026-04-07 00:46:52 -0300

    study: aula 74 - closure e funcoes que retornam outras funções

--- 02-python-intermediario/aula074.py
+++ 02-python-intermediario/aula074.py
@@ -1,11 +1,17 @@
+"""
+Closure e funções que retornam outras funções
+"""
+
 def criar_saudacao(saudacao):
     def saudar(nome):
         return f'{saudacao}, {nome}'
     return saudar
 
 falar_bom_dia = criar_saudacao('Bom dia')
+falar_boa_tarde = criar_saudacao('Boa tarde')
 falar_boa_noite = criar_saudacao('Boa noite')
 
-for nome in ['Maria', 'Joana', 'João']:
-    print(falar_bom_dia(nome))
-    print(falar_boa_noite(nome))
+print(falar_bom_dia('Maria'))
+for nomes in ['Julia', 'Henrique', 'Panda']:
+    print(falar_boa_tarde(nomes))
+print(falar_boa_noite('Pedro'))
